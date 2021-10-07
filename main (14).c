e53146ab6f042b4f30c21a38887589a7c16bc617
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-07 23:29:05 +0530

    Add files via upload

---
+++ main (14).c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main () 
+{
+int num;
+float f;
+
+scanf ("%d", &num);
+scanf ("%f", &f);
+
+printf ("\nEntered integer is: %d", num);
+printf ("\nEnter the float: %f:", f);
+return 0;
+}
