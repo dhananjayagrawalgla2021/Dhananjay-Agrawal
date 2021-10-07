b1165426edbc219fb3fb82e9003beccfe87a09f2
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-08 01:24:59 +0530

    Add files via upload

---
+++ main (28).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a,b;
+    printf("Enter 1 Side of Equilateral triangle");
+    scanf("%f", &a);
+    b=a*a*0.433;
+    printf("area is %f",b);
+
+    return 0;
+}
