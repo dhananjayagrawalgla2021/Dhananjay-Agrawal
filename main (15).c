85f42a834368ce11e766399be6faadcfa4dcd9f3
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-07 23:11:46 +0530

    Add files via upload

---
+++ main (15).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+*/
+
+#include <stdio.h>
+int main()
+{
+    float a,b,c;
+    
+    scanf("%f%f", &a,&b);
+    c=a+b;
+    printf("%f",c);
+
+    return 0;
+}
