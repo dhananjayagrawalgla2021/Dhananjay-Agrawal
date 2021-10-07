5368996a4162541bae8306ebfc95c63bd9062ea0
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-08 01:18:02 +0530

    Add files via upload

---
+++ main (27).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter Base and Height");
+    scanf("%d%d", &a,&b);
+    c=0.5*a*b;
+    printf("area is %d",c);
+
+    return 0;
+}
