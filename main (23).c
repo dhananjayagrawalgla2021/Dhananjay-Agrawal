40e0492ce2ff2627ae41b40bdb2ff6ea9d838cc9
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-08 00:57:07 +0530

    Add files via upload

---
+++ main (23).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int d,w,y;
+    scanf("%d", &d);
+    w=d/7;
+    y=d/365;
+    printf("\n Number of weeks %d \n Number of years %d", w,y);
+
+    return 0;
+}
