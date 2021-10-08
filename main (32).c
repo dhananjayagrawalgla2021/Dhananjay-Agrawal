6235e651127970b8ac87fb214efbc9e047518356
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-08 13:57:25 +0530

    Add files via upload

---
+++ main (32).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("this is a computer\n built in 2010\t in Aligarh district\v for the purpose of programming in C languagek\b.");
+
+    return 0;
+}
