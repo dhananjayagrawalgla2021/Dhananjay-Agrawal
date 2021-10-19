9fa6cc1d2e5d00fcd85d2ff61ebfbb62c7fc1fcb
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-19 19:45:55 +0530

    Add files via upload

---
+++ main (42).c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+    float f, lemda, c;
+    printf("enter speed then lemda\n");
+    scanf("%f%f", &c, &lemda);
+    f=(c/lemda);
+    printf("frequency =%f", f);
+    return 0;
+}
