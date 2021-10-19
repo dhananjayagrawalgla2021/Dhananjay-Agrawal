d4247628ef655529a6185f3fc3d5b0f601d87995
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-19 19:09:09 +0530

    Add files via upload

---
+++ main (44).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, t, u, vl, s, d;
+    a=4;
+    t=3;
+    u=0;
+    vl=(u+a*t);
+    d=(u*t)+(0.5*a*t*t);
+    printf("%d\n",vl);
+    printf("%d", d);
+    return 0;
+}
