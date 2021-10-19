d6c68f46f4cc0edb2ce8e83f9da727daf70e020a
dhananjayagrawalgla2021 <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-10-19 19:18:21 +0530

    Add files via upload

---
+++ main (43).c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int u, a, s;
+    float v;
+    u=30;
+    a=5;
+    s=70;
+    v=sqrt((u*u)+(2*a*s));
+    printf("%f", v);
+
+    return 0;
+}
