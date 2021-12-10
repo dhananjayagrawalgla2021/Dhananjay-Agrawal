181c36ab901fcca7a659836d6c89194b7761754a
Dhananjay Agrawal <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-12-10 21:40:49 +0530

    Add files via upload

---
+++ Array- Find union of two array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, i, j;
+    printf("a");
+    scanf("%d", &a);
+    printf("b");
+    scanf("%d", &b);
+    int x[a+1];
+    int y[b+1];
+    for(i=0; i<a; i++){
+        scanf("%d", &x[i]);
+    }
+    for(i=0; i<b; i++){
+        scanf("%d", &y[i]);
+    }
+    i=0; j=0;
+    while(i<a||j<b){
+        if(x[i]==y[j]){
+            printf("%d\t", x[i]);
+            i++; j++;
+        }
+        if(x[i]<y[j]){
+            printf("%d\t", x[i]);
+            i++;
+        }
+        if(x[i]>y[j]){
+            printf("%d\t", y[j]);
+            j++;
+        }
+    }
+    return 0;
+}
