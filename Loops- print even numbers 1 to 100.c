438a8aaa64d714db018e9829347cef79e57acd8b
Dhananjay Agrawal <92101381+dhananjayagrawalgla2021@users.noreply.github.com>
2021-12-20 19:31:25 +0530

    Add files via upload

---
+++ Loops- print even numbers 1 to 100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=1, b, n;
+   // scanf("%d", &n);
+    while(a<=100){
+        if(a%2==0){
+        printf("%d\t", a);}
+        a++;
+    }
+    return 0;
+}
